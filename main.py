ab46e62ec9b69379e0312f8d7ba24bc9791f2b3f
Greydi Lora <greydi.lora@yahoo.com>
2022-12-11 21:16:00 -0500

    Finish the app: Add sky option

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+# Widgets: title, text input, slider, select box, sub header
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place")
 days = st.slider("Forecast Days", min_value=1, max_value=5,help="Select the number of days by sliding")
@@ -13,7 +14,27 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for the next {days} day(s) in {place_cap}")
 
-d, t = get_data(place, days, option)
+if place:
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+    try:
+        # Get the temperature or Sky data
+        filtered_data = get_data(place, days)
+
+        if option == "Temperature":
+            # Filter data
+            temp = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temp, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write("Place does not exist, please try again.")
+
